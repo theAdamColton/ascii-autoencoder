1f263630598980cafb3408936352206d0988ae4a
Adam <atcolton@tutanota.com>
2022-05-09 16:15:48 -0600

    Fixed some issues with demonstration 	modified:   demonstration.py

--- demonstration.py
+++ demonstration.py
@@ -45,28 +45,32 @@ def main(stdscr, args):
     )
     z_dim = 256
 
-    autoenc = VanillaAutoenc(n_channels=95, z_dim=z_dim)
-    autoenc.load_state_dict(
-        torch.load(path.join(args.model_dir, "autoencoder.pth.tar"))
-    )
-    autoenc.eval()
-
     cuda = torch.cuda.is_available()
     if cuda:
-        autoenc.cuda()
         Tensor = torch.cuda.FloatTensor
         device = torch.device("cuda")
     else:
         Tensor = torch.FloatTensor
         device = torch.device("cpu")
 
+    autoenc = VanillaAutoenc(n_channels=95, z_dim=z_dim)
+    autoenc.load_state_dict(
+        torch.load(path.join(args.model_dir, "autoencoder.pth.tar"), map_location=device)
+    )
+    autoenc.eval()
+    if cuda:
+        autoenc.cuda()
+
+
     curses.noecho()
     curses.curs_set(False)
     pad = curses.newpad(80, 80)
 
     next_frame = time.time() + 1 / args.frame_rate
+    embedding2 = get_random(device, dataset)
     while True:
-        embedding1, embedding2 = get_random(device, dataset)
+        embedding1 = embedding2
+        embedding2 = get_random(device, dataset)
 
         for x in np.linspace(0, 1, 100):
 
@@ -75,25 +79,21 @@ def main(stdscr, args):
 
             x_scaled = np.log10(x ** args.smooth_factor + 1) * 3.322
             with torch.no_grad():
-                interp_embedding = x_scaled * embedding1 + (1 - x) * embedding2
+                interp_embedding = x_scaled * embedding2 + (1 - x) * embedding1
                 decoded = autoenc.decoder(interp_embedding.unsqueeze(0))
                 decoded_str = dataset.decode(decoded[0])
 
             pad.addstr(0, 0, decoded_str)
             pad.refresh(0, 0, 0, 0, 64, 64)
 
-            if x==0:
-                time.sleep(args.hold_length)
 
         time.sleep(args.hold_length)
 
 
 def get_random(device, dataset):
-    embedding1 = dataset[random.randint(0, len(dataset) - 1)][1]
-    embedding1 = embedding1.to(device)
-    embedding2 = dataset[random.randint(0, len(dataset) - 1)][1]
-    embedding2 = embedding2.to(device)
-    return embedding1, embedding2
+    embedding = dataset[random.randint(0, len(dataset) - 1)][1]
+    embedding = embedding.to(device)
+    return embedding
 
 
 if __name__ in {"__main__", "__console__"}:
