fixed a layer issue. this command gives a quickly converging solution bpython train.py --font-res 9 --font-zoom 21 --ce-recon-loss-scale 1.0 --kl-coeff 0.05 --image-recon-loss-coeff 50.0 --print-every 1 --run-name models/nov2022-gumbel-proper  --datapath ascii-dataset --n-workers 16 --learning-rate 2e-3 -b 128 -n 15000 --validation-prop 0.05 --validation-every 10 --space-deemph 100 --gumbel-tau-r 1e-4 --log-name lr2e3-wimloss-gt-1e4-sde5-dif-lay

autoencoder_models.py
@@ -47,8 +47,10 @@ class Decoder(nn.Module):
             BilinearConvUpsample(48, 64, kernel_size=kernel_size),
             # Input: batch_size by 64 by 64 by 64
             nn.Conv2d(64, n_channels, kernel_size, stride=1, padding=kernel_size // 2),
-            # Output: batch_size by 95 by 64 by 64
-            BilinearConvUpsample(95, 95, kernel_size=kernel_size, scale=1.0),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(95),
+            # Input: batch_size by 95 by 64 by 64
+            nn.Conv2d(n_channels, n_channels, kernel_size, stride=1, padding=kernel_size // 2),
             nn.Softmax2d()
         )
 